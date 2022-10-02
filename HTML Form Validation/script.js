f83d19a5830f1467594791a7cc9a45efeeca7059
Ahmed Ayaz <ahmedayaz.business@gmail.com>
2022-10-03 02:19:17 +0500

    Email Validation Completed

--- HTML Form Validation/script.js
+++ HTML Form Validation/script.js
@@ -11,6 +11,37 @@ userName.addEventListener('input' , () => {
     checkUserName(userName , 3 , 12);
 })
 
+email.addEventListener('input' , () => {
+    checkEmail(email);
+})
+
+let checkEmail = (field) => {
+    let text = field.value.trim();
+    // if user add space before username
+    field.value = text;
+    let expression = /^[\w#][\w\.\’+#](.[\w\\’#]+)\@[a-z]+[a-z]+[a-z]+[a-z]+(.[a-z]+[a-z]+[a-z])*(.[a-zA-Z]{2,20})$/;
+
+    // if email is empty
+    if(text === ''){
+        emailError.classList.add('error')
+        emailError.innerText = "\u26CC Please Enter Email";
+    }
+    // if email is invalid
+    else if(!expression.test(text)){
+        // if my errorTag already contains success
+        if(emailError.classList.contains('success')){
+            emailError.classList.remove('success');
+        }
+        emailError.classList.add('error')
+        emailError.innerText = "\u26CC Invalid Email";
+    }
+    else{
+        // if my email is Va;id
+        emailError.classList.add('success')
+        emailError.innerText = "\u2713 Valid Email";
+    }
+}
+
 let checkUserName = (field , min , max) => {
     let text = field.value.trim();
     // if user add space before username
