ba4d189e6fe4147c9dc2780974759de2c7c4f7cb
Ahmed Ayaz <ahmedayaz.business@gmail.com>
2022-10-03 02:09:27 +0500

    Username Validation Done

--- HTML Form Validation/script.js
+++ HTML Form Validation/script.js
@@ -1,21 +1,59 @@
 let labels = document.getElementsByClassName('Label');
 let credentails = document.getElementById('credentials')
 let userName =document.getElementById('userName');
+let email = document.getElementById('email');
 let userNameError =document.getElementById('username-error-msg');
+let emailError = document.getElementById('email-error-msg');
 
 // Username Validation
 
-userName.addEventListener('blur' , () => {
-    checkLength(userName , 3 , 12);
+userName.addEventListener('input' , () => {
+    checkUserName(userName , 3 , 12);
 })
 
-let checkLength = (field , min , max) => {
+let checkUserName = (field , min , max) => {
     let text = field.value.trim();
+    // if user add space before username
+    field.value = text;
+    // Validations
     if(text === ''){
+        if(userNameError.classList.contains('success')){
+            userNameError.classList.remove('success');
+        }
         userNameError.classList.add('error');
-        userNameError.innerText = "Error"
+        userNameError.innerText = "\u26CC Please Enter Username"
+    }
+    else if(text.length > max){
+        if(userNameError.classList.contains('success')){
+            userNameError.classList.remove('success');
+        }
+        userNameError.classList.add('error');
+        userNameError.innerText = "\u26CC Username must be less than 12 characters";
+    }
+    else if(text.length < 3){
+        if(userNameError.classList.contains('success')){
+            userNameError.classList.remove('success');
+        }
+        userNameError.classList.add('error');
+        userNameError.innerText = "\u26CC Username must be greater than 3 characters";
+    }
+    else if(/\d/g.test(text)){
+        if(userNameError.classList.contains('success')){
+            userNameError.classList.remove('success');
+        }
+        userNameError.classList.add('error');
+        userNameError.innerText = "\u26CC Numbers are not Allowed"
+    }
+    else{
+        // Correct Attempt after Wrong Attempts
+        if(userNameError.classList.contains('error')){
+            userNameError.classList.remove('error');
+        }
+        userNameError.classList.add('success')
+        userNameError.innerHTML = "\u2713 Username Available";
     }
 }
+
 
 // Input Animation
 
