6f0afee956325c3e96e56d9f0091f6862a56cf7d
Ahmed Ayaz <ahmedayaz.business@gmail.com>
2022-10-03 02:42:38 +0500

    Validation Completed Except PIN

--- HTML Form Validation/script.js
+++ HTML Form Validation/script.js
@@ -2,8 +2,10 @@ let labels = document.getElementsByClassName('Label');
 let credentails = document.getElementById('credentials')
 let userName =document.getElementById('userName');
 let email = document.getElementById('email');
+let pin = document.getElementById('pin');
 let userNameError =document.getElementById('username-error-msg');
 let emailError = document.getElementById('email-error-msg');
+let pinError = document.getElementById('pin-error-msg');
 
 // Username Validation
 
@@ -11,40 +13,9 @@ userName.addEventListener('input' , () => {
     checkUserName(userName , 3 , 12);
 })
 
-email.addEventListener('input' , () => {
-    checkEmail(email);
-})
-
-let checkEmail = (field) => {
+const checkUserName = (field , min , max) => {
     let text = field.value.trim();
-    // if user add space before username
-    field.value = text;
-    let expression = /^[\w#][\w\.\’+#](.[\w\\’#]+)\@[a-z]+[a-z]+[a-z]+[a-z]+(.[a-z]+[a-z]+[a-z])*(.[a-zA-Z]{2,20})$/;
-
-    // if email is empty
-    if(text === ''){
-        emailError.classList.add('error')
-        emailError.innerText = "\u26CC Please Enter Email";
-    }
-    // if email is invalid
-    else if(!expression.test(text)){
-        // if my errorTag already contains success
-        if(emailError.classList.contains('success')){
-            emailError.classList.remove('success');
-        }
-        emailError.classList.add('error')
-        emailError.innerText = "\u26CC Invalid Email";
-    }
-    else{
-        // if my email is Va;id
-        emailError.classList.add('success')
-        emailError.innerText = "\u2713 Valid Email";
-    }
-}
-
-let checkUserName = (field , min , max) => {
-    let text = field.value.trim();
-    // if user add space before username
+    // if user add space before and After Username
     field.value = text;
     // Validations
     if(text === ''){
@@ -61,7 +32,7 @@ let checkUserName = (field , min , max) => {
         userNameError.classList.add('error');
         userNameError.innerText = "\u26CC Username must be less than 12 characters";
     }
-    else if(text.length < 3){
+    else if(text.length < min){
         if(userNameError.classList.contains('success')){
             userNameError.classList.remove('success');
         }
@@ -82,6 +53,63 @@ let checkUserName = (field , min , max) => {
         }
         userNameError.classList.add('success')
         userNameError.innerHTML = "\u2713 Username Available";
+    }
+}
+
+
+// Email Validation
+
+email.addEventListener('input' , () => {
+    checkEmail(email);
+})
+
+const checkEmail = (field) => {
+    let text = field.value.trim();
+    // if user add space before and After Email
+    field.value = text;
+    let expression = /^[\w#][\w\.\’+#](.[\w\\’#]+)\@[a-z]+[a-z]+[a-z]+[a-z]+(.[a-z]+[a-z]+[a-z])*(.[a-zA-Z]{2,20})$/;
+
+    // if email is empty
+    if(text === ''){
+        if(emailError.classList.contains('success')){
+            emailError.classList.remove('success');
+        }
+        emailError.classList.add('error')
+        emailError.innerText = "\u26CC Please Enter Email";
+    }
+    // if email is invalid
+    else if(!expression.test(text)){
+        // if my errorTag already contains success
+        if(emailError.classList.contains('success')){
+            emailError.classList.remove('success');
+        }
+        emailError.classList.add('error')
+        emailError.innerText = "\u26CC Invalid Email";
+    }
+    else{
+        // if my email is Valid
+        emailError.classList.add('success')
+        emailError.innerText = "\u2713 Valid Email";
+    }
+}
+
+
+// Pin Validation
+
+pin.addEventListener('input' , () =>{
+    checkPin(pin);
+})
+
+const checkPin = (field) => {
+    let text = field.value.trim();
+    // if user add space before and After Pin
+    field.value = text;
+    if(text === ''){
+        if(pinError.classList.contains('success')){
+            pinError.classList.remove('success')
+        }
+        pinError.classList.add('error');
+        pinError.innerText = "\u26CC Please Enter Your Pin"
     }
 }
 
